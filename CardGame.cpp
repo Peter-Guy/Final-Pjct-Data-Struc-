28c63b5400723c90c01f63e4223324f7d71d4f6d
Symstep <128487262+Symstep@users.noreply.github.com>
2025-04-26 09:58:12 -0400

    Create main file

---
+++ CardGame.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include "deck.h"
+#include "crazy.h"
+using namespace std;
+
+int main() {
+	int gameChoice;
+
+	cout << "Welcome to the card games.\n";
+	
+	do {
+		cout << "1: War\n"
+			<< "2: Black Jack\n"
+			<< "3: Go Boom\n"
+			<< "4: Crazy Eights\n"
+			<< "5: 5 Card Stud\n"
+			<< "6: Rolling Stone\n"
+			<< "7: 98\n"
+			<< "8: Thirty One\n"
+			<< "9: Quit\n"
+			<< "Which game would you like to play: ";
+		cin >> gameChoice;
+		switch (gameChoice) {
+			case 1:
+				playWar();
+				break;
+			case 2:
+				playBlackJack();
+				break;
+			case 3:
+				playGoBoom();
+				break;
+			case 4:
+				playCrazyEight();
+				break;
+			case 5:
+				playFiveCardStud();
+				break;
+			case 6:
+				playRollingStone();
+				break;
+			case 7:
+				playNinetyEight();
+				break;
+			case 8:
+				playThirtyOne();
+				break;
+			case 9:
+				break;
+			default:
+				cout << "Invalid game choice.\n";
+		}
+	} while (gameChoice != 9);
+
+	return 0;
+}
